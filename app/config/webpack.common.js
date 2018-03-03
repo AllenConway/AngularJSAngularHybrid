12abb992b8a685c0064e574f7aeb3c5b15530f23
Allen Conway <allenc@magenic.com>
2018-03-03 00:05:25 -0500

    Initial Angular upgrade plus webpack configuration

---
+++ app/config/webpack.common.js
@@ -0,0 +1,79 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var helpers = require('./helpers');
+
+module.exports = {
+    //the entry-point files that define the bundles
+    entry: {
+        pollyfills: '../pollyfills.ts', //the polyfills needed to run Angular applications in most modern browsers
+        vendor: '../vendor.ts', //the third-party dependencies such as Angular, lodash, and bootstrap.css
+        app: '../main.ts' //the application code
+    },
+
+    //resolve extension-less file requests by looking for matching files with .ts extension or .js
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loaders: [
+                    {
+                        // loader to transpile the Typescript code to ES5, guided by the tsconfig.json file
+                        loader: 'awesome-typescript-loader',
+                        options: { configFileName: helpers.root('src', 'tsconfig.json') }
+                    }, 'angular2-template-loader' //loads angular components' template and styles
+                ]
+            },
+            {
+                //loads component templates
+                test: /\.html$/,
+                loader: 'html-loader'
+            },
+            {
+                //pattern matches application-wide styles
+                test: /\.css$/,
+                exclude: helpers.root('src', 'app'),
+                loader: ExtractTextPlugin.extract(
+                    {
+                        fallback: 'style-loader',
+                        use: 'css-loader?sourceMap'
+                    }
+                )
+            },
+            {
+                //pattern handles component-scoped styles (the ones specified in a component's styleUrls metadata property
+                test: /\.css$/,
+                include: helpers.root('src', 'app'),
+                loader: 'raw-loader'
+            }
+        ]
+    },
+
+    plugins: [
+        // Workaround for angular/angular#11580
+        new webpack.ContextReplacementPlugin(
+            // The (\\|\/) piece accounts for path separators in *nix and Windows
+            /angular(\\|\/)core(\\|\/)@angular/,
+            helpers.root('./app'), // location of your app
+            {} // a map of your routes
+        ),
+
+        //The CommonsChunkPlugin identifies the hierarchy among three chunks: app -> vendor -> polyfills. 
+        //Where Webpack finds that app has shared dependencies with vendor, it removes them from app.
+        //It would remove polyfills from vendor if they shared dependencies, which they don't
+        new webpack.optimize.CommonsChunkPlugin({
+            name: ['app', 'vendor', 'polyfills']
+        }),
+
+        //Webpack generates a number of js and CSS files. You could insert them into the index.html manually. 
+        //That would be tedious and error-prone. Webpack can inject those scripts and links for you with the HtmlWebpackPlugin
+        new HtmlWebpackPlugin({
+            template: '../../index.html'
+        })
+    ]
+
+};
---
+++ app/config/webpack.dev.js
@@ -0,0 +1,31 @@
+var webpackMerge = require('webpack-merge');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var commonConfig = require('./webpack.common.js');
+var helpers = require('./helpers');
+
+module.exports = webpackMerge(commonConfig, {
+  devtool: 'cheap-module-eval-source-map',
+
+  //Although you tell Webpack to put output bundles in the dist folder, the dev server keeps all bundles in memory; 
+  //it doesn't write them to disk. You won't find any files in the dist folder, at least not any generated from this development build
+  output: {
+    path: helpers.root('dist'),
+    //The HtmlWebpackPlugin, added in webpack.common.js, uses the publicPath and the filename settings to 
+    //generate appropriate <script> and <link> tags into the index.html
+    publicPath: '/',
+    filename: '[name].js',
+    chunkFilename: '[id].chunk.js'
+  },
+
+  //The CSS styles are buried inside the Javascript bundles by default. The ExtractTextPlugin extracts them into external .css 
+  //files that the HtmlWebpackPlugin inscribes as <link> tags into the index.html
+  plugins: [
+    new ExtractTextPlugin('[name].css')
+  ],
+
+  //The development build relies on the Webpack development server
+  devServer: {
+    historyApiFallback: true,
+    stats: 'minimal'
+  }
+});
