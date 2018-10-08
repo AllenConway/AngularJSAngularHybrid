d6917790bcb7e3be3f994278b922849e6ee89a18
Allen Conway <allenc@magenic.com>
2018-10-07 22:50:06 -0400

    Added unit testing in component with TypeMoq

--- app/engine/engine-horsepower.component.spec.ts
+++ app/engine/engine-horsepower.component.spec.ts
@@ -1,39 +1,64 @@
 import { TestBed } from '@angular/core/testing';
-import { EngineHorsepowerComponent } from './engine-horsepower.component';
 import { FormsModule } from '@angular/forms';
+
 import { CarService } from '../shared/services/car.service';
+import { EngineHorsepowerComponent } from './engine-horsepower.component';
+import { HorsepowerCalculations } from '../shared/model';
 import { HorsepowerCalculationsService } from '../shared/services/horsepower-calculations.service';
+import * as TypeMoq from "typemoq";
 
 describe('EngineHorsepowerComponent', () => {
 
-    beforeEach(() => {
+    let mockedHpCalcService = TypeMoq.Mock.ofType(HorsepowerCalculationsService);
+    let mockedCarService = TypeMoq.Mock.ofType(CarService);
+    let component: EngineHorsepowerComponent;
 
+    beforeEach(() => {
         TestBed.configureTestingModule({
-            imports:[
+            imports: [
                 FormsModule
             ],
             declarations: [
                 EngineHorsepowerComponent
             ],
             providers: [
-                CarService,
-                HorsepowerCalculationsService
+                { provide: HorsepowerCalculationsService, useFactory: () => { return mockedHpCalcService.object } },
+                { provide: CarService, useFactory: () => { return mockedCarService.object } }
             ]
         }).compileComponents();
     });
 
-    it('should create the app', () => {
-        const fixture = TestBed.createComponent(EngineHorsepowerComponent);
-        const app = fixture.debugElement.componentInstance;
-        expect(app).toBeTruthy();
+    beforeEach(() => {
+        let fixture = TestBed.createComponent(EngineHorsepowerComponent);
+        component = fixture.debugElement.componentInstance;
+    });
+
+    it('should create the component', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should call engineHorsepower from submit', () => {
+        
+        //Arrange
+        const horsepowerCalculationsMock: HorsepowerCalculations = {
+            rearWheelHorsepower: 500,
+            flywheelHorsepower: 575
+        }
+        const calculations = {
+            weight: null,
+            et: null 
+        };
+        mockedHpCalcService.setup(x => x.engineHorsepower(calculations)).returns(() => horsepowerCalculationsMock);
+
+        //Act
+        component.submit(calculations);
+
+        //Assert
+        mockedHpCalcService.verify(x => x.engineHorsepower(TypeMoq.It.isAny()), TypeMoq.Times.exactly(1));
+        expect(component.calculatedHP).toBeDefined();
+        expect(component.calculatedHP.rearWheelHorsepower).toEqual(500);
+        expect(component.calculatedHP.flywheelHorsepower).toEqual(575);
+
     });
 
 });
-
-// describe('simple matchers', function() {
-
-//     it("should be true", function () {
-//         expect(true).toBe(true);
-//     });
-
-// });
