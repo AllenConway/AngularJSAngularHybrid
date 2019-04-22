47432efb875a6226f64d146fb64ae195caae4d5b
Allen Conway <allenc@magenic.com>
2019-04-22 11:29:10 -0400

    -Updating Angular to v7 -Cleaning up unused packages -Added favicon

--- app/pollyfills.ts
+++ app/pollyfills.ts
@@ -1,11 +1,10 @@
-import 'core-js/es6';
-import 'core-js/es7/reflect';
+import 'core-js';
 require('zone.js/dist/zone');
 
 if (process.env.ENV === 'production') {
-// Production
+    // Production
 } else {
-// Development and test
-Error['stackTraceLimit'] = Infinity;
-require('zone.js/dist/long-stack-trace-zone');
+    // Development and test
+    Error['stackTraceLimit'] = Infinity;
+    require('zone.js/dist/long-stack-trace-zone');
 }
